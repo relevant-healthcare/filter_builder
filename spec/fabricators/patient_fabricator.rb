948ee69c5e54a4e84d0b6d54706343154f8a5eb2
Tassos Bareiss <tassosbareiss@gmail.com>
2017-11-28 15:07:49 -0500

    minor changes

--- spec/fabricators/patient_fabricator.rb
+++ spec/fabricators/patient_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator :patient do
-  first_name 'FirstName'
-  last_name  { sequence(:patient_name) { |i| "LastName #{i}" } }
+  first_name { sequence(:patient_fname) { |i| "FirstName #{i}" } }
+  last_name  { sequence(:patient_lname) { |i| "LastName #{i}" } }
   date_of_birth '1980-06-01'
-  # provider
+  provider
 end
--- spec/fabricators/provider_fabricator.rb
+++ spec/fabricators/provider_fabricator.rb
@@ -1,3 +1,3 @@
 Fabricator :provider do
-  npi { sequence(:provider_id) { |i| "npi #{i}" } }
+  npi { sequence(:provider_npi) { |i| "npi #{i}" } }
 end
