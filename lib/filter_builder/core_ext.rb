52abc49fe2dfe1589cbace64d9cf3fd556f47654
Tassos Bareiss <tassosbareiss@gmail.com>
2017-11-29 12:48:21 -0500

    conditionally use rails-specific methods if possible

--- lib/filter_builder/core_ext.rb
+++ lib/filter_builder/core_ext.rb
@@ -3,9 +3,7 @@ require 'filter_builder/filter'
 if Module.const_defined? 'ActiveRecord::Base'
   ActiveRecord::Base.class_eval do
     def self.filter(params)
-      where(id: FilterBuilder::Filter.new(self, params)
-                                     .scope
-                                     .select(:id))
+      FilterBuilder::Filter.new(self, params).scope
     end
   end
 end
--- lib/filter_builder/filter.rb
+++ lib/filter_builder/filter.rb
@@ -4,12 +4,13 @@ module FilterBuilder
 
     def initialize(filtered_class, params)
       @filtered_class = filtered_class
-      @params = params.stringify_keys
+      @params = params
     end
 
     def scope
-      params.inject(filtered_class.all) do |acc, (key, value)|
-        if filtered_class.reflections.include?(key) && value.is_a?(Hash)
+      params.inject(filtered_class.all) do |acc, (uncast_key, value)|
+        key = uncast_key.to_s
+        if join_and_recurse?(key, value)
           joined_class = filtered_class.reflections[key].klass
           acc.joins(key.to_sym).merge(Filter.new(joined_class, value).scope)
         elsif acc.respond_to?(key)
@@ -20,6 +21,13 @@ module FilterBuilder
           acc.where(key => value)
         end
       end
+    end
+
+    private
+
+    def join_and_recurse?(key, value)
+      filtered_class.respond_to?(:reflections) &&
+        filtered_class.reflections.include?(key) && value.is_a?(Hash)
     end
   end
 end
