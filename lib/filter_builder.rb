d6e81d0ae85ee4512e45905a68ff6eb6a9bc7b47
Tassos Bareiss <tassosbareiss@gmail.com>
2019-12-13 17:05:14 -0500

    Make require statements consistent

--- lib/filter_builder.rb
+++ lib/filter_builder.rb
@@ -8,9 +8,9 @@ require 'filter_builder/filter'
 require 'filter_builder/form'
 require 'filter_builder/core_ext'
 require 'filter_builder/operators/nil_operator'
+require 'filter_builder/operators/equals_operator'
+require 'filter_builder/operators/matches_case_insensitive_operator'
+require 'filter_builder/operators/matches_case_sensitive_operator'
 
 module FilterBuilder
-  require 'filter_builder/operators/equals_operator'
-  require 'filter_builder/operators/matches_case_insensitive_operator'
-  require 'filter_builder/operators/matches_case_sensitive_operator'
 end
