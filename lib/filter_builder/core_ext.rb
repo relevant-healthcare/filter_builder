748975bb102705ced784898ef27d592e30057381
Billy Kaplan <billy@relevant.healthcare>
2020-12-08 17:18:25 -0500

    Add filterbuilder_filter method

--- lib/filter_builder/core_ext.rb
+++ lib/filter_builder/core_ext.rb
@@ -1,6 +1,11 @@
 if Module.const_defined? 'ActiveRecord::Base'
   ActiveRecord::Base.class_eval do
+    # DEPRECATED: use ActiveRecord.filterbuilder_filter instead
     def self.filter(params)
+      self.filterbuilder_filter(params)
+    end
+
+    def self.filterbuilder_filter(params)
       FilterBuilder::Filter.new(self, params).scope
     end
   end
