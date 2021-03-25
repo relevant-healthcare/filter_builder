cf23a263633f4e0d079240a570b4a2e60708d697
Derrick Theodore <derrick@relevant.healthcare>
2021-03-25 15:13:13 -0400

    migrated #filter -> #filterbuilder_filter - updated README - updated MIT-LICENSE from commit 708fbec1a240c9dc0df2064dca06710d51b64474

--- lib/filter_builder/conditions.rb
+++ lib/filter_builder/conditions.rb
@@ -10,67 +10,67 @@ module FilterBuilder
     end
 
     class Equals < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where(field.name => value)
       end
     end
 
     class DoesNotEqual < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where.not(field.name => value)
       end
     end
 
     class MatchesCaseInsensitive < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} ~* ?", value)
       end
     end
 
     class DoesNotMatchCaseInsensitive < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} !~* ?", value)
       end
     end
 
     class MatchesCaseSensitive < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} ~ ?", value)
       end
     end
 
     class DoesNotMatchCaseSensitive < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} !~ ?", value)
       end
     end
 
     class Gt < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} > ?", value)
       end
     end
 
     class Lt < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} < ?", value)
       end
     end
 
     class Gte < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} >= ?", value)
       end
     end
 
     class Lte < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} <= ?", value)
       end
     end
 
     class Between < Condition
-      def filter(scope)
+      def filterbuilder_filter(scope)
         scope.where("#{field.namespaced} BETWEEN :min AND :max", value)
       end
     end
